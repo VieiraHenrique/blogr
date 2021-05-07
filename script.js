5ad4e95e574ba2bb357ddf7c9911303539caf419
Henrique Andrade Vieira <me.henriquevieira@gmail.com>
2021-05-07 20:18:05 +0200

    test

---
+++ script.js
@@ -0,0 +1,9 @@
+console.log("inside JS");
+
+var docWidth = document.documentElement.offsetWidth;
+
+[].forEach.call(document.querySelectorAll("*"), function (el) {
+  if (el.offsetWidth > docWidth) {
+    console.log(el);
+  }
+});
